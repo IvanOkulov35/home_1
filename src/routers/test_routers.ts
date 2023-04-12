d52e029f237a47920512bced9d4cd9048f4ee1c8
Ivan Okulov <okulov35@mail.ru>
2023-04-12 21:10:29 +0300

    add  delete

--- src/routers/test_routers.ts
+++ src/routers/test_routers.ts
@@ -1,15 +1,15 @@
 import {Router} from "express";
 import {app} from "../index";
 import {videos} from "./router_video";
-import {HTTP_STATUS} from "./router_video";
+
 
 export  const testRouters = Router({})
-testRouters.delete('/all-data', (req,res) => {
+testRouters.delete('/', (req,res) => {
     videos.splice(0,videos.length);
     if(videos.length === 0) {
-        res.sendStatus(HTTP_STATUS.NO_CONTENT_204)
+        res.sendStatus(204)
     }
     else {
-        res.sendStatus(HTTP_STATUS.METHOD_NOT_ALLOWED)
+        res.sendStatus(405)
     }
 })
